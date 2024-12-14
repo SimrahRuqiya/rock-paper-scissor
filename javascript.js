a424cd526c18c7882af51d512b0c828afd02e201
SimrahRuqiya <123015484+SimrahRuqiya@users.noreply.github.com>
2024-12-14 20:56:46 +0400

    cases of win + loss + tie works

--- javascript.js
+++ javascript.js
@@ -1,17 +1,45 @@
+let score=0;
+let humanScore=0;
+let computerScore=0;
+
 function getComputerChoice() {
     let val=Math.floor(Math.random() * 3)
     if (val === 0) {
-        return "Computer: Rock"
+        return "Rock";
     } else if (val === 1) {
-        return "Computer: Paper"
+        return "Paper";
     } else if (val === 2) {
-        return "Computer: Scissor"
+        return "Scissor";
     }
 }
 
 function getHumanChoice() {
     let answer = prompt("Choose R/P/S(write full name pls): ");
-    return "You: " + answer;
+    return answer;
 }
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Paper"){
+        console.log("Loss");
+    } else if (humanChoice === "Rock" && computerChoice === "Scissor") {
+        console.log("Win");
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("Win");
+    } else if (humanChoice === "Paper" && computerChoice === "Scissor") {
+        console.log("Loss");
+    } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
+        console.log("Loss");
+    } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
+        console.log("Win");
+    } else {
+        console.log("Tie");
+    }
+}
+
+const humanSelection=getHumanChoice();
+const computerSelection=getComputerChoice();
+
+console.log("Bot: " + computerSelection)
+console.log("Human: " + humanSelection)
+
+playRound(humanSelection, computerSelection);
